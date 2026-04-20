11f39965b9ed396b36978f1b67602231a6834b0e
sByrons <109484176+sByrons@users.noreply.github.com>
2026-04-19 19:48:07 -0600

    Vistas de Configuracion y Reportes
    Vista de Configuracion y Reportes

---
+++ Controllers/ConfiguracionController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiservicioB.Controllers
+{
+	public class ConfiguracionController : Controller
+	{
+		public IActionResult Roles() => View();
+		public IActionResult Servicios() => View();
+		public IActionResult Estados() => View();
+		public IActionResult Materiales() => View();
+		public IActionResult Parametros() => View();
+	}
+}
+
---
+++ Controllers/ReportesController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiservicioB.Controllers
+{
+	public class ReportesController : Controller
+	{
+		public IActionResult Indicadores() => View();
+		public IActionResult ReporteFechas() => View();
+		public IActionResult DesempenoTecnicos() => View();
+		public IActionResult Satisfaccion() => View();
+		public IActionResult Exportar() => View();
+	}
+}
